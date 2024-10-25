39e109c3b2d26023b883e0943ce5e76ae4cb7ba3
hola-official <sam@overthetopseo.com>
2024-10-25 13:13:43 +0100

    commit

--- script/DegenToken.ts
+++ script/DegenToken.ts
@@ -1,55 +1,37 @@
-const hre = require("hardhat");
+import hre from "hardhat";
+import { ethers } from "hardhat";
 
 async function main() {
   const myAccount = "0x429cB52eC6a7Fc28bC88431909Ae469977F6daCF";
-
-  const deployer = await hre.ethers.getSigner(myAccount);
-  // const [deployer] = await hre.ethers.getSigners();
-
+  const deployer = await ethers.getSigner(myAccount);
   const DEPLOYED_CONTRACT = "0xD018054CaD63CAA0c1e0982d6c85c2e5A5CdaEC4";
+  const amt = ethers.parseUnits("100", 18);
 
   console.log("Deploying DegenToken contract...");
-  const degenToken = await hre.ethers.getContractAt(
+  const degenToken = await ethers.getContractAt(
     "DegenToken",
     DEPLOYED_CONTRACT
   );
-  // const degenToken = await DegenToken.deploy();
-//  degenToken.wait();
 
-  console.log("DegenToken deployed to:", degenToken);
+  console.log("DegenToken deployed to:", degenToken.target);
 
-  // Check the initial balance of the deployer
-  let deployerBalance = await degenToken.balanceOf(deployer);
+  let deployerBalance = await degenToken.balanceOf(deployer.address);
+  const mint = await degenToken.mint(deployer.address,amt)
+  mint.wait();
+
   console.log(
     "Deployer balance after minting:",
-    hre.ethers.formatUnits(deployerBalance, 18)
+    ethers.formatUnits(deployerBalance, 18)
   );
 
-  // Retrieve all items available in the store
-  const itemCount = await degenToken.items.length;
-  console.log("Items available in the store:");
-
-  for (let i = 0; i < itemCount; i++) {
-    const item = await degenToken.items(i);
-    console.log(
-      `Item ${i + 1}:`,
-      item.name,
-      "-",
-      hre.ethers.utils.formatUnits(item.cost, 18),
-      "DEGEN"
-    );
-  }
-
-  // Redeem two items from the store (example: items 0 and 1)
   console.log("Redeeming items...");
-  await degenToken.redeemItem(0); // Redeem "Health"
-  await degenToken.redeemItem(1); // Redeem "Skins"
+  await degenToken.redeemItem(0);
+  await degenToken.redeemItem(1);
 
-  // Get the balance before and after redeeming items
-  deployerBalance = await degenToken.balanceOf(deployer);
+  deployerBalance = await degenToken.balanceOf(deployer.address);
   console.log(
     "Deployer balance after redemption:",
-    hre.ethers.utils.formatUnits(deployerBalance, 18)
+    ethers.formatUnits(deployerBalance, 18)
   );
 
   console.log("Redemption complete. Events are logged on-chain.");
@@ -58,6 +40,6 @@ async function main() {
 main()
   .then(() => process.exit(0))
   .catch((error) => {
-    console.error(error);
+    console.error("Error:", error);
     process.exit(1);
   });
